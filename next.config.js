2d2b91da86b11cc9a7b30de5feb0a6a51791fd06
Shashvat Seth <sethshashvat18@gmail.com>
2026-09-05 05:56:19 +0530

    fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   reactStrictMode: true,
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
   webpack: (config) => {
